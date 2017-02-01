2d4236d3879e11ece2236a0ab184b6010c7dec09
YLife <359059275@qq.com>
2017-02-01 21:38:58 +0800

    update

---
+++ Test/src/test/Test1.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test1 {
+	public static void main(String[] args) {
+		short a = 1;
+		a = (short) (a+1);
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+	}
+}
---
+++ Test/src/test/Test2.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test2 {
+	public static void main(String[] args) {
+		final StringBuffer sb = new StringBuffer("hello");
+		sb.append("a");
+		System.out.println(sb);
+	}
+}
